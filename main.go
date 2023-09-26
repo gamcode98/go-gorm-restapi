141fa1072661af6cb4d3e7d96289c8a1d89fd183
Gabriel Alejandro Mamani <gamcode98@gmail.com>
2023-09-26 18:44:07 -0300

    crud

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gamcode98/go-gorm-restapi/db"
+	"github.com/gamcode98/go-gorm-restapi/models"
 	"github.com/gamcode98/go-gorm-restapi/routes"
 	"github.com/gorilla/mux"
 )
@@ -11,9 +12,22 @@ import (
 func main() {
 	db.DBConnection()
 
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
+
+	r.HandleFunc("/tasks", routes.GetTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
+	r.HandleFunc("/tasks", routes.CreateTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id}", routes.DeleteTaskHandler).Methods("DELETE")
+
+	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	r.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
+	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
 
 	http.ListenAndServe(":3000", r)
 }
--- routes/index.routes.go
+++ routes/index.routes.go
@@ -3,5 +3,5 @@ package routes
 import "net/http"
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	w.Write([]byte("REST API WORKING"))
 }
